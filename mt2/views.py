15068675b20f298579910003f89911935da1696b
maximovalencia <chilab@inacap.cl>
2023-09-22 11:41:00 -0300

    app2 actualizada

--- mt2/views.py
+++ mt2/views.py
@@ -4,5 +4,9 @@ from django.http import HttpResponse
 def vistaDos (request):
     html="""
     <h1>App 2 funcionando correctamente</h1>
+    <h2>Aguante colo colo</h2>
+    <h3>LocoLoco</h3>
+    <h4>Vio</h4>
+
     """
     return HttpResponse(html)
