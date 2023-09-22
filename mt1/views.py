7357db5d5f764c62da89c44f689fd1a8fa3e0fd4
maximovalencia <chilab@inacap.cl>
2023-09-22 11:44:22 -0300

    app1 actualizada etiqueta

--- mt1/views.py
+++ mt1/views.py
@@ -4,5 +4,8 @@ from django.http import HttpResponse
 def vistaUno (request):
     html="""
     <h1>App 1 funcionando correctamente</h1>
+    <h2>Valoranchi</h2>
+    <h3>completando etiqueta jaja</h3>
+    <h4>Lo quiero profe^^</h4>
     """
     return HttpResponse(html)
