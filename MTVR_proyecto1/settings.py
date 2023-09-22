4bd891b5648c23aa8c778b9d8cb51de7a1dc78bb
maximovalencia <chilab@inacap.cl>
2023-09-22 11:06:12 -0300

    app1 terminada

--- MTVR_proyecto1/settings.py
+++ MTVR_proyecto1/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mt1',
 ]
 
 MIDDLEWARE = [
---
+++ mt1/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Mt1Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mt1'
