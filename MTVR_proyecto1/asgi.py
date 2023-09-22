c6d24836655368781cfc40566055201a16e661cb
maximovalencia <chilab@inacap.cl>
2023-09-22 10:57:56 -0300

    proyecto base

---
+++ MTVR_proyecto1/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for MTVR_proyecto1 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MTVR_proyecto1.settings')
+
+application = get_asgi_application()
